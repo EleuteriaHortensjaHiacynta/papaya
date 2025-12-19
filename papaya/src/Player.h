c0161870035d4765c2244a7d8eee41ca96144e98
Maqu0 <108890670+Maqu0@users.noreply.github.com>
2025-12-19 16:02:28 +0100

    Added state machine, animation engine and lerp camera.  Updated assets.

--- papaya/src/Player.h
+++ papaya/src/Player.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 // Pomocnicza funkcja do p³ynnej zmiany wartoœci (+- Lerp)
+// Mo¿emy jej u¿ywaæ jako efekt uboczny dzia³ania jakiegoœ artefaktu czy cuœ
 inline float Approach(float current, float target, float increase) {
 	if (current < target)
 		return std::min(current + increase, target);
@@ -11,6 +12,15 @@ inline float Approach(float current, float target, float increase) {
 	else
 		return std::max(current - increase, target);
 }
+
+enum AnimState {
+	IDLE,
+	WALK,
+	JUMP_UP,
+	JUMP_DOWN,
+	TURN
+};
+
 
 class Player {
 public:
@@ -20,8 +30,8 @@ public:
 
 	// Do zmian
 	float maxSpeed = 100.0f;
-	float acceleration = 10000.0f;
-	float friction = 9998.0f;
+	float acceleration = 1110.0f;
+	float friction = 990.0f;
 	float gravity = 500.0f;
 	float jumpForce = 250.0f;
 
@@ -39,10 +49,17 @@ public:
 	Rectangle frameRec;
 
 	// Zmienne animacji
-	int currentFrame = 0;
+	AnimState currentState = IDLE;
+
+	// Konfiguracja klatek
+	int startFrame = 0;
+	int lengthFrames = 0; // ile klatek trwa animacja
+	bool loop = true; // czy animacja siê powtarza?
+
+	// Liczniki
+	int currentFrameRelative = 0;
 	float frameTimer = 0.0f;
 	float frameSpeed = 0.1f;
-	int totalFrames = 6;
 
 	// Kierunek patrzenia
 	bool isFacingRight = true;
@@ -52,7 +69,7 @@ public:
 		position = { startX, startY };
 		velocity = { 0, 0 };
 
-		texture = LoadTexture("Assets/player_walk.png");
+		texture = LoadTexture("Assets/player.png");
 
 		frameRec = { 0.0f, 0.0f, 16.0f, 16.0f };
 
@@ -84,12 +101,22 @@ public:
 		if (IsKeyDown(KEY_RIGHT)) moveInput = 1.0f;
 		if (IsKeyDown(KEY_LEFT)) moveInput = -1.0f;
 
+		// ruch poziomy z pominiêciem akceleracji
+		if (moveInput != 0) {
+			velocity.x = moveInput * maxSpeed;
+		}
+		else {
+			velocity.x = 0;
+		}
+
+
+		//Kod do akceleracji
 		// ruch poziomy
+		/*
 		float targetSpeed = moveInput * maxSpeed;
 
 		float currentAccel;
 		float currentFriction;
-
 		if (isGrounded) {
 			// na ziemi
 			currentAccel = 4000.0;
@@ -104,6 +131,8 @@ public:
 		float accelRate = (moveInput != 0) ? currentAccel : currentFriction;
 
 		velocity.x = Approach(velocity.x, targetSpeed, accelRate * deltaTime);
+		*/
+
 
 		// skok
 		if (jumpBufferCounter > 0 && coyoteTimeCounter > 0)
@@ -114,8 +143,9 @@ public:
 			isGrounded = false;
 		}
 
-		if (!jumpHeld && velocity.y < 0) {
-			velocity.y = Approach(velocity.y, 0, friction * deltaTime);
+		if (!jumpHeld && velocity.y < -50.0f) {
+			velocity.y = -50.0f;
+			//velocity.y = Approach(velocity.y, 0, friction * deltaTime);
 		}
 
 		// grawitacja
@@ -157,34 +187,92 @@ public:
 			}
 		}
 
-		// sprawdzamy w któr¹ stronê patrzy postaæ
-		if (velocity.x > 0) isFacingRight = true;
-		else if (velocity.x < 0) isFacingRight = false;
+		// STATE MACHINE
+		if (moveInput != 0.0f) {
 
-		// czy siê rusza?
-		bool isMoving = (abs(velocity.x) > 10.0f);
+			bool wantsGoRight = (moveInput > 0);
 
-		if (isMoving && isGrounded) {
-			// animacja biegania
-			frameTimer += deltaTime;
-				
-			if (frameTimer >= frameSpeed) {
-				frameTimer = 0.0f;
-				currentFrame++;
-					
-				// zapêtlanie animacji
-				if (currentFrame >= totalFrames) currentFrame = 0;
+			if (wantsGoRight != isFacingRight) {
+				isFacingRight = wantsGoRight;
+
+				if (currentState != TURN && isGrounded) {
+					currentState = TURN;
+					currentFrameRelative = 0;
+				}
 			}
-
-		}
-		else {
-			// gracz stoi lub skacze
-			currentFrame = 0;
 		}
 
-		// przesuwanie "okienko wycinania" na odpowiedni¹ klatkê
-		frameRec.x = (float)currentFrame * 16.0f;
+		bool isBusyTurning = (currentState == TURN && currentFrameRelative < 4 && isGrounded); //animacja obrotu ma 5 klatek (0-4)
 
+		if (!isBusyTurning) {
+			if (!isGrounded) {
+				if (velocity.y < 0) {
+					currentState = JUMP_UP;
+				}
+				else {
+					currentState = JUMP_DOWN;
+				}
+			}
+			else {
+				if (moveInput != 0.0f) {
+					currentState = WALK;
+				}
+				else {
+					currentState = IDLE;
+				}
+			}
+		}
+
+
+		// mapa
+
+		switch (currentState) {
+		case WALK:
+			startFrame = 0; lengthFrames = 6; loop = true; frameSpeed = 0.1f;
+			break;
+
+		case JUMP_UP:
+			startFrame = 6; lengthFrames = 3; loop = false; frameSpeed = 0.1f;
+			break;
+
+		case JUMP_DOWN:
+			startFrame = 9; lengthFrames = 3; loop = false; frameSpeed = 0.1f;
+			break;
+
+		case IDLE:
+			startFrame = 12; lengthFrames = 6; loop = true; frameSpeed = 0.15f;
+			break;
+
+		case TURN:
+			startFrame = 18; lengthFrames = 3; loop = false; frameSpeed = 0.04f;
+			break;
+		}
+
+		// Silnik animacji
+
+		frameTimer += deltaTime;
+
+		if (frameTimer >= frameSpeed) {
+			frameTimer = 0.0f;
+			currentFrameRelative++;
+
+			if (currentFrameRelative >= lengthFrames) {
+				// obs³ugiwanie koñca animacji
+				if (loop) {
+					currentFrameRelative = 0;
+				}
+				else {
+					currentFrameRelative = lengthFrames - 1;
+
+					if (currentState == TURN) {
+						currentState = (moveInput != 0) ? WALK : IDLE;
+						currentFrameRelative = 0;
+					}
+				}
+			}
+		}
+		// finalne przesuniêcie okienka animacji
+		frameRec.x = (float)(startFrame + currentFrameRelative) * 16.0f;
 	}
 
 	//potrzebne do kolizji
