045e1f18341e5f1057ae4cb68081f4a52c89b995
Nimgleer <admin@DESKTOP-JCOS1B9>
2025-12-19 16:02:28 +0100

    Added state machine, animation engine and lerp camera.  Updated assets.

--- papaya/src/Scenes/scene_test_room.h
+++ papaya/src/Scenes/scene_test_room.h
@@ -13,6 +13,9 @@ public:
 	Player* player;
 	std::vector<Platform> platforms;
 
+
+	Camera2D camera = { 0 };
+
 	// Konstruktor
 	SceneTestRoom() {
 		// inicjalizacja gracza (do pozycji startowej)
@@ -20,8 +23,13 @@ public:
 
 		// TWORZENIE POZIOMU
 		platforms.push_back({ {0,160,320,40},GRAY }); // pod³oga
-		platforms.push_back({ {100,110,80,10},DARKGRAY }); // lewa platforma
+		platforms.push_back({ {100,110,80,10},DARKPURPLE }); // lewa platforma
 		platforms.push_back({ {220,70,60,10},BLACK }); // prawa platforma
+
+		camera.target = { player->position.x, player->position.y }; // na co patrzy kamera
+		camera.offset = { 320.0f / 2.0f, 180.0f / 2.0f }; // punkt œrodka na ekranie
+		camera.rotation = 0.0f;
+		camera.zoom = 1.0f;
 	}
 
 	// Destruktor
@@ -44,17 +52,34 @@ public:
 			player->position = { 30,100 };
 			player->velocity = { 0,0 };
 		}
+
+		Vector2 targetPos = { player->position.x + player->size.x / 2, player->position.y + player->size.y / 2 };
+
+		camera.target.x += (targetPos.x - camera.target.x) * 5.0f * deltaTime;
+		camera.target.y += (targetPos.y - camera.target.y) * 5.0f * deltaTime;
+
+
 	}
 
 	void Draw() {
 		//rysowanie t³a
-		ClearBackground(RAYWHITE);
+		ClearBackground(DARKGRAY);
+
+		Camera2D renderCam = camera;
+
+		renderCam.target.x = floor(camera.target.x);
+		renderCam.target.y = floor(camera.target.y);
+		// ¿eby nie by³o sub-pixel rendering zaokr¹glamy float w lerpie do inta
+
+		BeginMode2D(renderCam);
 
 		for (const auto& p : platforms) {
 			DrawRectangleRec(p.rect, p.color);
 		}
 
 		player->Draw();
+
+		EndMode2D();
 
 		// Debug info
 		DrawText("Sterowanie: Strza³ki + Spacja", 5, 5, 5, DARKGRAY);
