7d4b04cacafbe239dc5e2cc656317e3856184beb
EleuteriaHortensjaHiacynta <axel.loboda@gmail.com>
2025-12-23 00:11:25 +0100

    implemented and hopefully mostly finished the grid class for easier layout creation
    - added the grid class
    - created a widget class
    - changed the button to use the widget class
    
    - used polimorphism to make the grid accept any widget to make the code cleaner and hopefully easier to use

--- papaya/src/GUI/GUIRayLib.hpp
+++ papaya/src/GUI/GUIRayLib.hpp
@@ -5,118 +5,220 @@
 #include <utility>
 #include <string>
 #include <vector>
+#include <iostream>
+
+class Widget {
+public:
+	//using virtual makes it so when using the draw function using a pointer 
+	//the function from the class is used instead of the widgets
+	void virtual draw() = 0;
 
 
-class Button {
+	//allows for setting the position of a widget after its creation
+	void virtual setPosition(Rectangle rect) = 0;
+
+	//from what im understanding this makes the destructor delete both widget and the derived classes when using delete
+	virtual ~Widget() = default;
+};
+
+class Button : public Widget {
 private:
 	std::function<void()> mPassedFunction;
 
-
 public:
-	Rectangle PositionSize;
-	Vector2 MousePos;
-	
-	Color ColourBase;
-	Color ColourHover;
-	Color ColourClick;
-	Color TextColour;
-	
-	std::string Text;
-	int FontSize;
-	int TextLength;
+	Rectangle positionSize;
+	Vector2 mousePos;
 
-	int TextX;
-	int TextY;
+	Color colourBase;
+	Color colourHover;
+	Color colourClick;
+	Color textColour;
 
-	Button(Rectangle POSITIONSIZE, Color Base, Color Hover, Color Click) {
-		PositionSize = POSITIONSIZE;
-		ColourBase = Base;
-		ColourHover = Hover;
-		ColourClick = Click;
+	std::string text;
+	int fontSize;
+	int textLength;
+
+	int textX;
+	int textY;
+
+	Button(Rectangle positionSize, Color base, Color hover, Color click) {
+		this->positionSize = positionSize;
+		colourBase = base;
+		colourHover = hover;
+		colourClick = click;
 	}
 
-	void addText(std::string PassedText, int PassedFontSize, Color PassedTextColour) {
-		Text = PassedText;
-		TextLength = MeasureText(Text.c_str(), PassedFontSize);
-		TextColour = PassedTextColour;
-		FontSize = PassedFontSize;
-		TextX = PositionSize.x + (PositionSize.width - TextLength) / 2;
-		TextY = PositionSize.y + (PositionSize.height - FontSize) / 2;
+	void addText(std::string passedText, int passedFontSize, Color passedTextColour) {
+		text = passedText;
+		textLength = MeasureText(text.c_str(), passedFontSize);
+		textColour = passedTextColour;
+		fontSize = passedFontSize;
+		textX = positionSize.x + (positionSize.width - textLength) / 2;
+		textY = positionSize.y + (positionSize.height - fontSize) / 2;
 	}
 
-	void draw(Color Colour) {
-		DrawRectangleRec(PositionSize, Colour);
-		DrawText(Text.c_str(), TextX, TextY, FontSize, TextColour);
+	void render(Color colour) {
+		DrawRectangleRec(positionSize, colour);
+		DrawText(text.c_str(), textX, textY, fontSize, textColour);
 	}
 
-	//the template allows for passing a function of any type
-	// and also allows passing any number of arguments of any type for that function
 	template<typename Funct, typename... Args>
-	// && allows to pass lvalues and rvalues which i dont exactly know what they are but i think it needs to be like this
-	// gotta read more about them
 	void storeFunction(Funct&& function, Args&&... args) {
-
-		//all i know is that the compiler only accepted it like this and not when it was all done without a tuple
-		//inside the lambda function AND needed a change from compiling using the c++14 standard to c++17
-		//ill look into it later
 		auto argsTuple = std::make_tuple(std::forward<Args>(args)...);
 
-		//the lambda function that im almost sure i understand now
-		//almost
-		mPassedFunction = [function = std::forward<Funct>(function), argsTuple = std::move(argsTuple)]() mutable {
+		mPassedFunction =
+			[function = std::forward<Funct>(function),
+			argsTuple = std::move(argsTuple)]() mutable {
 			std::apply(function, argsTuple);
 			};
 	}
 
 	void detectMouseInteraction() {
-		MousePos = GetMousePosition();
-		//control output
-		std::cout << MousePos.x << " " << MousePos.y << std::endl;
-		if (CheckCollisionPointRec(MousePos, PositionSize)) {
+		mousePos = GetMousePosition();
+
+		// control output
+		std::cout << mousePos.x << " " << mousePos.y << std::endl;
+
+		if (CheckCollisionPointRec(mousePos, positionSize)) {
 			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-				draw(ColourClick);
+				render(colourClick);
 				if (mPassedFunction) {
 					mPassedFunction();
 				}
 			}
 			else {
-				draw(ColourHover);
+				render(colourHover);
 			}
 		}
 		else {
-			draw(ColourBase);
+			render(colourBase);
 		}
 	}
-	
+
+
+	void Widget::draw() override {
+		detectMouseInteraction();
+	}
+
+	void Widget::setPosition(Rectangle rect) override {
+		positionSize.x = rect.x;
+		positionSize.y = rect.y;
+		positionSize.width = rect.width;
+		positionSize.height = rect.height;
+	}
+};
+
+struct Cell {
+	Rectangle rect;
+
+	std::shared_ptr<Widget> widget;
 
 };
 
-class Grid {
+//Grid also inherits from Widget class so that it can hold subgrids
+
+class Grid : public Widget{
 private:
-	const int Rows;
-	const int RowHeight;
+	const int mRows;
+	int rowHeight;
 
-	const int Columns;
-	const int ColumnWidth;
+	const int mColumns;
+	int columnWidth;
 
-	std::vector < std::vector < Rectangle > > Cells;
+
+	//from what i read there's no need to use delete later 
+	// because this pointer doesnt own the parent grid so it shouldnt even delete it
+	Grid* mpParentGrid = nullptr;
+
+	
 
 public:
+	std::vector<std::vector<Cell>> cells;
+	// top left corner is the origin of the grid, everything gets set relative to it
+	int originX;
+	int originY;
 
-	//top left corner is the origin of the grid, everything gets set relative to it
-	int OriginX;
-	int OriginY;
-	
-	Grid(int PassedRows, int PassedColumns, int PassedRowHeight, int PassedColumnWidth,
-		int GridOriginX, int GridOriginY)
-		: Rows(PassedRows), Columns(PassedColumns),
-		RowHeight(PassedRowHeight), ColumnWidth(PassedColumnWidth) 
+	Grid(int passedRows,
+		int passedColumns,
+		int passedRowHeight,
+		int passedColumnWidth,
+		int gridOriginX,
+		int gridOriginY)
+		: mRows(passedRows),
+		mColumns(passedColumns),
+		rowHeight(passedRowHeight),
+		columnWidth(passedColumnWidth),
+		cells(mRows, std::vector<Cell>(mColumns))
 	{
-		OriginX = GridOriginX;
-		OriginY = GridOriginY;
 
+		giveCoordinates(gridOriginX, gridOriginY);
 
 	}
 
+	void giveCoordinates(int xPos, int yPos) {
+		originX = xPos;
+		originY = yPos;
 
+		for (int i = 0; i < mRows; i++) {
+			for (int j = 0; j < mColumns; j++) {
+				cells[i][j].rect = {
+					(float)(originX + j * columnWidth),
+					(float)(originY + i * rowHeight),
+					(float)columnWidth,
+					(float)rowHeight
+				};
+			}
+		}
+	}
+
+	void setParent(Grid* parent) {
+		mpParentGrid = parent;
+	}
+
+	bool isSubgrid() {
+		if (mpParentGrid == nullptr) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
+	void Widget::draw() override {
+		for (int i = 0; i < mRows; i++) {
+			for (int j = 0; j < mColumns; j++) {
+				if (cells[i][j].widget != nullptr) {
+					cells[i][j].widget->draw();
+				}
+				else {
+					DrawRectangleLinesEx(cells[i][j].rect, 1, GRAY);
+				}
+			}
+		}
+	}
+
+	void Widget::setPosition(Rectangle rect) override {
+		originX = rect.x;
+		originY = rect.y;
+		giveCoordinates(originX, originY);
+	}
+
+	void insertWidget(int row, int column, std::shared_ptr<Widget> passedWidget) {
+		if (row < mRows && row >= 0 && column < mColumns && column >= 0) {
+			cells[row][column].widget = passedWidget;
+		}
+	}
+
+
+	//should only works when its a subgrid
+	void expandSubgridToFillCell() {
+		if (isSubgrid() == true) {
+			columnWidth = mpParentGrid->columnWidth / mColumns;
+			rowHeight = mpParentGrid->rowHeight / mRows;
+
+			giveCoordinates(originX, originY);
+		}
+
+	}
+	
 };
