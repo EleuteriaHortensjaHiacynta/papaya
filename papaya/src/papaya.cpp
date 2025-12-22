32f1ac0fe8d8b268c5898a138cb3076b0dc405b8
EleuteriaHortensjaHiacynta <axel.loboda@gmail.com>
2025-12-22 15:53:17 +0100

    Finished the button class at least for now. Started creating a grid class for layout
    - Added the ability to add text to the button that gets automatically centered
    - started creating a grid class to help with layout
    - changed .h  files to .hpp so github doesnt read it as c code anymore

--- papaya/src/papaya.cpp
+++ papaya/src/papaya.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <raylib.h>
-#include "Scenes/MainMenu.h"
+#include "Scenes/MainMenu.hpp"
 
 
 int main() {
@@ -14,6 +14,7 @@ int main() {
     const int WindowHeight = 720;
     const int WindowWidth = 1280;
     char State = 0;
+    bool ShouldQuit = false;
 
     InitWindow(WindowWidth, WindowHeight, "papaya");
 
@@ -24,13 +25,13 @@ int main() {
     // because thats the way i found to easily declare things once and not in every iteration
     // and this also makes it so for example the mouse position is only checked when required and not always
 
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose() && !ShouldQuit) {
         switch (State) {
         case 0:
-            MainMenu(MousePos, WindowWidth, WindowHeight);
+            sceneMainMenu(MousePos, WindowWidth, WindowHeight, ShouldQuit);
             break;
         default:
-            MainMenu(MousePos, WindowWidth, WindowHeight);
+            sceneMainMenu(MousePos, WindowWidth, WindowHeight, ShouldQuit);
         }
     }
 
@@ -38,14 +39,3 @@ int main() {
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
