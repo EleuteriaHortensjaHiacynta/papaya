37cec7a511c152cd493f53ee171af6771c68345b
EleuteriaHortensjaHiacynta <axel.loboda@gmail.com>
2026-01-04 00:40:20 +0100

    scrolling tile selector
    -added a library to handle selecting which files to save to and load from
    - added a scrollable tile selector

--- papaya/src/level_editor/functions_level_editor.hpp
+++ papaya/src/level_editor/functions_level_editor.hpp
@@ -2,8 +2,21 @@
 
 #include <raylib.h>
 #include <iostream>
-#include <vector>
+#include <string>
+#include "external_headers/tinyfiledialogs.hpp"
 
-void fetchTexture(int ID) {
-	return;
+inline std::string openFileDialog(const char *tittle = "Select file to open") {
+	const char *result = tinyfd_openFileDialog(tittle, "", 0, nullptr, nullptr, 0);
+	if (result) return std::string(result);
+	return "";
+}
+
+// either all arguments need default values or none of them have defaults
+inline std::string saveFileDialog(const char* tittle = "Select file to save", int x = 0, int y = 0) {
+
+	std::string currentPath = std::filesystem::current_path().string();
+	std::string defaultName = currentPath + "/chunk" + std::to_string(x) + "-" + std::to_string(y) + ".json";
+	const char* result = tinyfd_saveFileDialog(tittle, defaultName.c_str(), 0, nullptr, nullptr);
+	if (result) return std::string(result);
+	return "";
 }
