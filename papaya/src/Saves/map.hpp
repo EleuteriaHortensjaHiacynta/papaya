6cbbfcde540ec704efe522597a425705ce77a10e
Kacper Łapka <kacper.lapka@gmail.com>
2026-01-06 20:12:41 +0100

    Create map.hpp file

---
+++ papaya/src/Saves/map.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <cstdint>
+#include <fstream>
+#include <vector>
+#include "../Entities/Wall.h"
+
+#define BLOCK_SIZE (sizeof(int16_t) + sizeof(int16_t) + sizeof(uint8_t) + sizeof(uint8_t) + sizeof(uint8_t) + sizeof(uint8_t))
+
+enum class Layers : uint8_t {
+    GUI = 0,
+    PLAYER = 1,
+    ENEMIES = 2,
+    BACKGROUND = 3,
+};
+
+enum class ExtraData : uint8_t {
+    NONE = 0,
+    COLLIDABLE = 1,
+    DAMAGING = 2,
+    INTERACTIVE = 3,
+};
+
+struct Block {
+    uint16_t x;
+    uint16_t y;
+    uint8_t x_length;
+    uint8_t y_length;
+    int8_t textureID;
+    Layers layer;
+    ExtraData extraData;
+};
+
+uint64_t createBlock(Block block);
+Block decodeBlock(uint64_t blockData);  
+class MapSaver {
+private:
+    std::fstream* fileStream = nullptr;
+public:
+    explicit MapSaver(std::fstream& f) : fileStream(&f) {}
+    void addBlock(Block block);
+    void sortBlocksByX();
+    ~MapSaver() {}
+};
+
+class MapLoader {
+private:
+    std::fstream* fileStream = nullptr;
+public:
+    explicit MapLoader(std::fstream& f) : fileStream(&f) {}
+    std::vector<Wall> getAll();
+    ~MapLoader() {}
+};
